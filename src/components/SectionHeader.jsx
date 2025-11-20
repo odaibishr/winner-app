12f1e75f2f8e0e114affced553649894fc3611c3
Odai Bishr <odaibishr@gmail.com>
2025-11-20 21:43:06 +0300

    added SectionHeader component

---
+++ src/components/SectionHeader.jsx
@@ -0,0 +1,10 @@
+const SectionHeader = ({ title, description }) => {
+	return (
+		<div className="md:h-[220px] h-[150px] flex text-center flex-col gap-5 items-center rounded-[80px] justify-center green-gradient ">
+			<h2 className="text-primary text-4xl max-sm:text-2xl">{title}</h2>
+			<p className="text-white text-2xl max-sm:text-lg">{description}</p>
+		</div>
+	);
+};
+
+export default SectionHeader;
--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,7 +1,14 @@
+import SectionHeader from "../components/SectionHeader";
+
 const PrivacyPolicy = () => {
 	return (
-		<div>
-			<h1>سياسة الخصوصية</h1>
+		<div className="relative h-screen w-full bg-gradient pt-30 md:pt-28 pb-20 md:pb-10 overflow-hidden">
+			<section className="px-10">
+				<SectionHeader
+					title="سياسة الخصوصية"
+					description="نحن ملتزمون بحماية خصوصيتك وبيانتك الشخصية"
+				/>
+			</section>
 		</div>
 	);
 };
