080be9cb5897f1ea6f35014dc4c451dbabc3b831
Odai Bishr <odaibishr@gmail.com>
2025-11-20 21:22:04 +0300

    create privacy policy page and route

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./index.css";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
+			<Route path="/privacy-policy" element={<PrivacyPolicy />} />
 		</Routes>
 	);
 }
---
+++ src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,9 @@
+const PrivacyPolicy = () => {
+	return (
+		<div>
+			<h1>سياسة الخصوصية</h1>
+		</div>
+	);
+};
+
+export default PrivacyPolicy;
