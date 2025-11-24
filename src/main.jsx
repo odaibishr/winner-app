a9ccd0f085c0c77a9ee21685ca6cef4f9c570aba
Odai Bishr <odaibishr@gmail.com>
2025-11-24 19:33:48 +0300

    added scroll to top when navigatioin

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<BrowserRouter>
+			<ScrollToTop behavior="smooth" />
 			<Navbar />
 			<App />
 		</BrowserRouter>
