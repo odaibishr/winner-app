43746eb45884dc783af68f421e8e1b059433bffa
Odai Bishr <odaibishr@gmail.com>
2025-11-24 19:05:36 +0300

    added border with shadow to features & vision mission sections

--- src/components/VisionMissionSection.jsx
+++ src/components/VisionMissionSection.jsx
@@ -3,19 +3,21 @@ import HorizontailInfoCard from "./HorizontailInfoCard";
 
 const VisionMissionSection = () => {
 	return (
-		<section className="md:py-20 py-16 container flex flex-col md:gap-20 gap-16">
-			<HorizontailInfoCard
-				index={0}
-				image="/vision-image.png"
-				title="رؤيـــتـنـــا"
-				text="أن نكون المنصة الرائدة للتواصل الاجتماعي في المنطقة العربية، نربط بين الثقافات ونقرب المسافات بتقنيات مبتكرة وتجربة استخدام استثنائية."
-			/>
-			<HorizontailInfoCard
-				index={1}
-				image="/mission-image.png"
-				title="مهــمتــنــــا"
-				text="تطوير تطبيق تواصل اجتماعي يجمع بين السهولة والأمان والمتعة، مع ميزات فريدة مثل نظام الهدايا والغرف التفاعلية لتعزيز التواصل الإنساني."
-			/>
+		<section className="md:py-20 py-16 border border-b-white md:shadow-2xl shadow-lg shadow-white/50">
+			<div className="container flex flex-col md:gap-20 gap-16">
+				<HorizontailInfoCard
+					index={0}
+					image="/vision-image.png"
+					title="رؤيـــتـنـــا"
+					text="أن نكون المنصة الرائدة للتواصل الاجتماعي في المنطقة العربية، نربط بين الثقافات ونقرب المسافات بتقنيات مبتكرة وتجربة استخدام استثنائية."
+				/>
+				<HorizontailInfoCard
+					index={1}
+					image="/mission-image.png"
+					title="مهــمتــنــــا"
+					text="تطوير تطبيق تواصل اجتماعي يجمع بين السهولة والأمان والمتعة، مع ميزات فريدة مثل نظام الهدايا والغرف التفاعلية لتعزيز التواصل الإنساني."
+				/>
+			</div>
 		</section>
 	);
 };
