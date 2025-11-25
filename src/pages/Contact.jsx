d2a3459ecae17e40a66a892f304c64de6e6fb6ae
Odai Bishr <odaibishr@gmail.com>
2025-11-26 00:05:52 +0300

    added header section to contact and creat label for form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,14 @@
+import SectionHeader from "../components/SectionHeader.jsx";
+
 const Contact = () => {
     return (
         <main className="bg-black pt-32 md:pt-40 lg:pt-48 min-h-screen">
-
+            <div className="py-10 border-2 border-b-secondary/40 container">
+                <SectionHeader
+                    title="تواصــل معنــا"
+                    description="نحن هنا للإجابة على استفساراتك ومساعدتك. لا تتردد في التواصل معنا"
+                />
+            </div>
         </main>
     );
 }
