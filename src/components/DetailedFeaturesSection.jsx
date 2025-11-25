0d76a19c157f2311d9dbff1d89fd9b80783b75ce
Odai Bishr <odaibishr@gmail.com>
2025-11-25 21:02:29 +0300

    create detailed features section

---
+++ src/components/DetailedFeaturesSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DetailedFeaturesSection = () => {
+  return (
+      <section className='py-16 md:py-20 section-border'>
+          
+    </section>
+  )
+}
+
+export default DetailedFeaturesSection
