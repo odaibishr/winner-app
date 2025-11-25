1270469d0546e517fc16fbce9334c677b72844db
Odai Bishr <odaibishr@gmail.com>
2025-11-25 22:45:15 +0300

    added tecnical specifications data

--- src/components/TechnicalSpecificationsSection.jsx
+++ src/components/TechnicalSpecificationsSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SectionHeader from "./SectionHeader";
+import { technicalSpecificationsData } from "../utils/constant";
 
 const TechnicalSpecificationsSection = () => {
 	return (
@@ -8,6 +9,14 @@ const TechnicalSpecificationsSection = () => {
 				title="المواصفات التقنية"
 				description="تقنيات متقدمة لتجربة استخدام مثالية"
 			/>
+
+			<div className="py-10  bg-primary/50">
+				<div className="container grid sm:grid-cols-2 items-center justify-center md:grid-cols-2 lg:grid-cols-4 gap-y-6 md:gap-6">
+					{technicalSpecificationsData.map((feature) => (
+						<FeaturedCard key={feature.id} {...feature} />
+					))}
+				</div>
+			</div>
 		</section>
 	);
 };
