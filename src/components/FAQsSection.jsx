86aec59b6e1ecb2cdbf7d65f5c34d97c29d37de1
Odai Bishr <odaibishr@gmail.com>
2025-11-27 20:05:41 +0300

    added section header to faqs section

--- src/components/FAQsSection.jsx
+++ src/components/FAQsSection.jsx
@@ -1,6 +1,9 @@
+import SectionHeader from "./SectionHeader.jsx";
+
 const FAQsSection = () => {
     return (
-        <section className="py-10 md:py-20">
+        <section className="py-10 md:py-20 container">
+            <SectionHeader title="الأسئلة الشائعة؟؟" description="إجابات سريعة على الأسئلة الأكثر شيوعاً" />
 
         </section>
     );
