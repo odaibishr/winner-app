51c814edf956e0de946dd4c71951e8fdbe37a9df
Odai Bishr <odaibishr@gmail.com>
2025-11-27 19:06:50 +0300

    make the form resposive and add title icon to the form

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -3,9 +3,12 @@ import ContactForm from "./ContactForm.jsx";
 const FormSection = () => {
     return (
       <section className="py-10 md:py-20 container">
-          <div className="bg-primary/50 rounded-4xl p-10">
-              <div className="w-full h-full bg-white p-10 rounded-2xl text-center">
-                  <h1 className="text-2xl md:text-4xl text-secondary font-medium mb-10">ارسل لنا رسالة</h1>
+          <div className="bg-primary/50 rounded-4xl p-5 md:p-10">
+              <div className="w-full h-full bg-white p-5 md:p-10 rounded-2xl text-center">
+                  <h1 className="mb-10 flex items-center justify-center gap-1">
+                      <img src="/message.svg" alt="Message" className="hidden md:block w-12 h-12" />
+                      <span className="text-xl md:text-4xl text-secondary font-medium ">أرســـل لنـــــا رســــالــة...</span>
+                  </h1>
                   <ContactForm />
               </div>
           </div>
